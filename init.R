2b7a7d2e79ed16f14c35bd00b264dfce151acc6a
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-09-19 23:52:21 +0200

    Add files via upload

---
+++ init.R
@@ -0,0 +1,17 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c("shiny", "shinydashboard", "shinycssloaders", "shinyanimate", "shinydashboardPlus",
+                "shinyEffects", "shinybusy", "shinyBS", "magrittr", "readr", 
+                "tidyverse", "ggplot2", "dplyr", "magrittr", "formattable", "fontawesome",
+                "haven", "caret", "h2o", "rsample", "DT", "plotly", "purrr", "readr", "shinyWidgets",
+                "tibble", "utils")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
