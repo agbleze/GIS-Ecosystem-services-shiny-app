01c685b7ef47e30236ce469ac0a69e957d7645fb
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-09-20 23:55:52 +0200

    Added UI code

---
+++ ui.R
@@ -0,0 +1,54 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Ecosystem service app"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(
+                tabPanel(title = h3(strong(em("Ecosystem Services"))),
+                         tabsetPanel(
+                             tabPanel(title = "Economic Ecosystem Services",
+                                      fluidPage(
+                                          fluidRow(
+                                          #    leafletOutput("leaflet_test"),
+                                              leafletOutput("test2")
+                                          )
+                                      )),
+                             tabPanel(title = "Ecological Ecosystem Services"),
+                             tabPanel(title = "Social Ecosystem Services"))),
+                tabPanel(title = h3(strong(em("Prioritization")))),
+                tabPanel(title = h3(strong(em("Geocomputations"))),
+                         tabsetPanel(
+                             tabPanel(title = "Hotspot Analysis"),
+                             tabPanel(title = "Spatial Autocorrelation"),
+                             tabPanel(title = "Clustering"),
+                             tabPanel(title = "Modelling")
+                         ))
+            )
+        )
+    )
+))
