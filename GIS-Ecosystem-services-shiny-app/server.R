0f0256b452f37d7456590ffbc9c2414b34677154
agbleze <62272559+agbleze@users.noreply.github.com>
2021-09-24 01:00:14 +0200

    new

--- GIS-Ecosystem-services-shiny-app/server.R
+++ GIS-Ecosystem-services-shiny-app/server.R
@@ -8,10 +8,82 @@
 #
 
 library(shiny)
+library(leaflet)
+library(viridis)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
+shinyServer(function(input, output, session) {
+        sync(m1, m2, m3, m4)
+ output$economic_es <- renderMapview({
+     # leaflet() %>%
+     #     addTiles() %>%
+     #     addRasterImage(economic_es_mean_raster, colors = pal, opacity = 0.5) %>%
+     #     addLegend(values = values(economic_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+         
+         mapview::mapview(norm_economic_es_mean_clip1_raster, layer.name = "Economic Ecosystem Services")
+ })
  
+ output$ecological_es <- renderMapview({
+     # leaflet() %>%
+     #     addTiles() %>%
+     #     addRasterImage(ecological_es_mean_raster, colors = pal) %>%
+     #     addLegend(values = values(ecological_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+     #     
+         mapview::mapview(ecological_es_mean_raster, layer.name = "Ecological Ecosystem Services")
+ })
+ 
+ output$social_es <- renderMapview({
+     # leaflet() %>%
+     #     addTiles() %>%
+     #     addRasterImage(social_es_mean_raster, colors = pal) %>%
+     #     addLegend(values = values(social_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+     #     
+         mapview::mapview(social_es_mean_raster, layer.name = "Social Ecosystem Services")
+ })
+ 
+ output$economic_priori <- renderMapview({
+     mapview::mapview(norm_economic_prioritized_raster, layer.name = "Economic prioritization")
+ })
+ 
+ output$ecological_priori <- renderMapview({
+     mapview::mapview(norm_ecological_prioritized_raster, layer.name = "Ecological prioritization", 
+                      query.type = 'mousemove', 
+                      query.digits = 2)
+ })
+ 
+ output$social_priori <- renderMapview({
+     mapview::mapView(norm_social_prioritized_raster, layer.name = "Social prioritization") + 
+                 mapview::mapview(fran_adm1_shp, zcol = "NAME_1", alpha.regions = 0, legend = FALSE,
+                                  layer.name = "Regions of France")
+ })
+ 
+ 
+#  output$equal_priori_hotspot <- renderMapview({
+#          #m1
+#          mapview(equal_priori_hotspot_raster, query.type = 'mousemove', 
+#                  query.digits = 2) + mapview(economic_priori_hotspot_raster, query.type = 'mousemove',
+#                                              query.digits = 2) 
+# 
+#          #sync(m1, m2, m3, m4)
+#  })
+#  
+#  output$economic_priori_hotspot <- renderMapview({
+#          m2
+# })
+#  
+#  output$ecological_priori_hotspot <- renderMapview(m3)
+#  output$social_priori_hotspot <- renderMapview(m4)
+ 
+ # output$try <- renderMapview(
+ #         mapview(equal_priori_hotspot_raster, query.type = 'mousemove', 
+ #                       query.digits = 2) + mapview(economic_priori_hotspot_raster, query.type = 'mousemove',
+ #                       query.digits = 2) + mapview(ecological_priori_hotspot_raster, query.type = 'mousemove',
+ #                       query.digits = 2) + mapview(social_priori_hotspot_raster, query.type = 'mousemove',
+ #                       query.digits = 2) 
+ # )
+ 
+ output$hotspot <- renderUI({
+         sync(list(m1, m2, m3, m4)) 
+ })
 
 })
--- GIS-Ecosystem-services-shiny-app/ui.R
+++ GIS-Ecosystem-services-shiny-app/ui.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(leaflet)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -28,7 +29,82 @@ shinyUI(fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(
-                tabPanel(title = "Ecosystem Services")
+                tabPanel(title = h3(strong(em("Ecosystem Services"))),
+                         tabsetPanel(
+                             tabPanel(title = "Economic Ecosystem Services",
+                                      fluidPage(
+                                          fluidRow(
+                                          #    leafletOutput("leaflet_test"),
+                                              mapviewOutput("economic_es")
+                                          )
+                                      )),
+                             tabPanel(title = "Ecological Ecosystem Services",
+                                      fluidPage(
+                                          fluidRow(
+                                            #  leafletOutput("ecological_es")
+                                              mapviewOutput("ecological_es")
+                                          )
+                                      )),
+                             tabPanel(title = "Social Ecosystem Services",
+                                      fluidPage(
+                                          fluidRow(
+                                            #  leafletOutput("social_es")
+                                              mapviewOutput("social_es")
+                                          )
+                                      )
+                                      )
+                             )),
+                tabPanel(title = h3(strong(em("Prioritization"))),
+                         tabsetPanel(
+                             tabPanel(title = "Economic Prioritization",
+                                 fluidPage(
+                                     fluidRow(
+                                         mapviewOutput("economic_priori")
+                                     )
+                             )
+                             
+                         ),
+                         tabPanel(title = "Ecological Prioritization",
+                             fluidPage(
+                                 fluidRow(
+                                     mapviewOutput("ecological_priori")
+                                 ) 
+                             )
+                         ),
+                         tabPanel(title = "Social Prioritization",
+                                  fluidPage(
+                                      fluidRow(
+                                          mapviewOutput("social_priori")
+                                      )
+                                  )
+                                  )
+                         
+                         )),
+                tabPanel(title = h3(strong(em("Geocomputations"))),
+                         tabsetPanel(
+                             tabPanel(title = "Hotspot Analysis",
+                                      fluidPage(
+                                          uiOutput("hotspot")
+                                          )
+                                      #     fluidRow(
+                                      #        column(width = 6, mapviewOutput("equal_priori_hotspot")),
+                                      #        column(width = 6, mapviewOutput("economic_priori_hotspot"))
+                                      #     )
+                                      # ),
+                                      # fluidPage(
+                                      #     fluidRow(
+                                      #         column(width = 6, mapviewOutput("ecological_priori_hotspot")),
+                                      #         column(width = 6, mapviewOutput("social_priori_hotspot"))
+                                      #     )
+                                      # )
+                                      ),
+                             tabPanel(title = "Spatial Autocorrelation",
+                                      fluidPage(
+                                          uiOutput("try")
+                                      )),
+                             tabPanel(title = "Clustering"),
+                             tabPanel(title = "Modelling")
+                         ))
             )
         )
     )
