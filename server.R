281ce7dd10ba613322ef66285af31cd86f339080
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-09-30 08:47:47 +0200

    Update server.R

--- server.R
+++ server.R
@@ -13,37 +13,103 @@ library(viridis)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
- 
- output$economic_es <- renderLeaflet({
-     leaflet() %>%
-         addTiles() %>%
-         addRasterImage(economic_es_mean_raster, colors = pal, opacity = 0.5) %>%
-         addLegend(values = values(economic_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+        sync(m1, m2, m3, m4)
+ output$economic_es <- renderMapview({
+     # leaflet() %>%
+     #     addTiles() %>%
+     #     addRasterImage(economic_es_mean_raster, colors = pal, opacity = 0.5) %>%
+     #     addLegend(values = values(economic_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+         
+         mapview::mapview(norm_economic_es_mean_clip1_raster, layer.name = "Economic Ecosystem Services")
  })
  
- output$ecological_es <- renderLeaflet({
-     leaflet() %>%
-         addTiles() %>%
-         addRasterImage(ecological_es_mean_raster, colors = pal) %>%
-         addLegend(values = values(ecological_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+ output$ecological_es <- renderMapview({
+     # leaflet() %>%
+     #     addTiles() %>%
+     #     addRasterImage(ecological_es_mean_raster, colors = pal) %>%
+     #     addLegend(values = values(ecological_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+     #     
+         mapview::mapview(ecological_es_mean_raster, layer.name = "Ecological Ecosystem Services")
  })
  
- output$social_es <- renderLeaflet({
-     leaflet() %>%
-         addTiles() %>%
-         addRasterImage(social_es_mean_raster, colors = pal) %>%
-         addLegend(values = values(social_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+ output$social_es <- renderMapview({
+     # leaflet() %>%
+     #     addTiles() %>%
+     #     addRasterImage(social_es_mean_raster, colors = pal) %>%
+     #     addLegend(values = values(social_es_mean_raster), pal = pal, title = "Amount (Normalized)")
+     #     
+         mapview::mapview(social_es_mean_raster, layer.name = "Social Ecosystem Services")
  })
  
- output$economic_priori <- renderTmap({
-     hotspot_economic_priori_tm
+ output$economic_priori <- renderMapview({
+     mapview::mapview(norm_economic_prioritized_raster, layer.name = "Economic prioritization")
  })
  
  output$ecological_priori <- renderMapview({
-     mapview::mapview(ecological_prioritized_raster, query.type = 'mousemove', 
-                      query.digits = 2, trim = TRUE, na.opacity = 1)
- }
-    
- )
+     mapview::mapview(norm_ecological_prioritized_raster, layer.name = "Ecological prioritization", 
+                      query.type = 'mousemove', 
+                      query.digits = 2)
+ })
+ 
+ output$social_priori <- renderMapview({
+     mapview::mapView(norm_social_prioritized_raster, layer.name = "Social prioritization") + 
+                 mapview::mapview(fran_adm1_shp, zcol = "NAME_1", alpha.regions = 0, legend = FALSE,
+                                  layer.name = "Regions of France")
+ })
+ 
+ 
+#  output$equal_priori_hotspot <- renderMapview({
+#          #m1
+#          mapview(equal_priori_hotspot_raster, query.type = 'mousemove', 
+#                  query.digits = 2) + mapview(economic_priori_hotspot_raster, query.type = 'mousemove',
+#                                              query.digits = 2) 
+# 
+#          #sync(m1, m2, m3, m4)
+#  })
+#  
+#  output$economic_priori_hotspot <- renderMapview({
+#          m2
+# })
+#  
+#  output$ecological_priori_hotspot <- renderMapview(m3)
+#  output$social_priori_hotspot <- renderMapview(m4)
+ 
+ # output$try <- renderMapview(
+ #         mapview(equal_priori_hotspot_raster, query.type = 'mousemove', 
+ #                       query.digits = 2) + mapview(economic_priori_hotspot_raster, query.type = 'mousemove',
+ #                       query.digits = 2) + mapview(ecological_priori_hotspot_raster, query.type = 'mousemove',
+ #                       query.digits = 2) + mapview(social_priori_hotspot_raster, query.type = 'mousemove',
+ #                       query.digits = 2) 
+ # )
+ 
+ output$hotspot <- renderUI({
+         sync(list(m1, m2, m3, m4)) 
+ })
+ 
+ ###### Geocomputations
+ output$correlation_es <- renderPlot({
+         raster::pairs(all_es_mean_stack)
+ })
+ 
+ output$economic_spa_autocorr <- renderValueBox({
+        # h4("Spatial autocorrelation")
+         valueBox(value = h2(formattable::comma(economic_moran, digits = 3), style = "background: green"), 
+                  subtitle = "Economic Ecosystem Services" 
+                  # href = "https://agbleze.github.io/Profile/index.html", 
+                  # HTML("style = color: blue; background: green")
+         )
+ })
+ 
+ output$ecological_spa_autocorr <- renderValueBox({
+         valueBox(value = h2(formattable::comma(ecological_moran, digits = 3), style = "background: green"), 
+                  subtitle = "Ecological Ecosystem Services")
+ })
+ 
+ output$social_spa_autocorr <- renderValueBox({
+         valueBox(value = h2(formattable::comma(social_moran, digits = 3), style = "background: green"),
+                  subtitle = "Social Ecosystem Services"
+                  )
+ })
 
 })
+
